a473e495b4d4007d5443f52181224ade7ed143ad
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-02-26 20:52:04 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,14 @@
+function solution(num1, num2) {
+    var answer = 0;
+	if(num1 ===num2){
+        answer = 1;
+        return answer;
+    }
+    else{
+        answer = -1;
+        return answer;
+    }
+    
+    return 0;
+    
+}
